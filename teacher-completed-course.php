e6a91ce84ef787f3053209175e70296c01e5472f
Mike Wing <michael.wing@rocketmail.com>
2017-08-07 09:54:08 +0100

    Create teacher-completed-course.php
    Adds details into the completed course email template from a Sensei Learners ID number.
    Can be customised to get any meta data from a user

---
+++ teacher-completed-course.php
@@ -0,0 +1,45 @@
+<?php
+/**
+ * Teacher completed course email
+ *
+ * @author WooThemes
+ * @package Sensei/Templates/Emails/HTML
+ * @version 1.6.0
+ */
+if ( ! defined( 'ABSPATH' ) ) exit; // Exit if accessed directly ?>
+
+<?php
+
+// Get data for email content
+global $sensei_email_data;
+extract( $sensei_email_data );
+
+global $wp_user_query;
+extract( $wp_user_query ); 
+
+// For gmail compatibility, including CSS styles in head/body are stripped out therefore styles need to be inline. These variables contain rules which are added to the template inline. !important; is a gmail hack to prevent styles being stripped if it doesn't like something.
+$small = "text-align: center !important;";
+
+$large = "text-align: center !important;font-size: 350% !important;line-height: 100% !important;";
+
+?>
+
+<?php do_action( 'sensei_before_email_content', $template ); ?>
+
+<p style="<?php echo esc_attr( $small ); ?>"><?php _e( 'Your student', 'woothemes-sensei' ); ?></p>
+
+<h2 style="<?php echo esc_attr( $small ); ?>"><?php echo 'Unique ID:', $learner_id; ?></h2>
+
+<h2 style="<?php echo esc_attr( $large ); ?>"><?php $user = get_user_by( 'id', $learner_id ); echo '' . $user->first_name . ' ' . $user->last_name; ?></h2>
+
+<h2 style="<?php echo esc_attr( $small ); ?>"><?php echo 'NOK Number:'; $user = get_user_by( 'id', $learner_id ); echo $user->nok_number; ?></h2>
+
+<p style="<?php echo esc_attr( $small ); ?>"><?php printf( __( 'has completed and %1$s the course', 'woothemes-sensei' ), $passed ); ?></p>
+
+<h2 style="<?php echo esc_attr( $large ); ?>"><?php echo get_the_title( $course_id ); ?></h2>
+
+<hr/>
+
+<p style="<?php echo esc_attr( $small ); ?>"><?php printf( __( 'Manage this course\'s inductees %1$shere%2$s.', 'woothemes-sensei' ), '<a href="http://veja-mate.smchse.com/inductees/">', '</a>' ); ?></p>
+
+<?php do_action( 'sensei_after_email_content', $template ); ?>
